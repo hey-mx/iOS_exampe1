57c6963fdf1ce3dfd0fe77d971edc6a44ef9af57
Juan Carlos Juarez <isc.jcjl@gmail.com>
2013-10-16 21:11:57 -0500

    Primera parte del curso

--- iMejorando/ViewController.m
+++ iMejorando/ViewController.m
@@ -9,7 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+    @property (strong, nonatomic) NSURLSession *session;
+    @property (strong, nonatomic) NSURLSessionConfiguration *sessionConfiguration;
 @end
 
 @implementation ViewController
@@ -18,6 +19,35 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    self.menuItems = [[NSMutableArray alloc] init];
+    
+    NSURL *url = [NSURL URLWithString: @""];
+    NSURLRequest *request = [NSURLRequest requestWithURL: url];
+    self.sessionConfiguration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    self.session = [NSURLSession sessionWithConfiguration: self.sessionConfiguration];
+    NSURLSessionDataTask *task = [self.session dataTaskWithRequest: request
+        completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+            NSHTTPURLResponse *urlResponse = (NSHTTPURLResponse *)response;
+            if(urlResponse.statusCode == 200) {
+                NSLog(@"YESSSSS");
+                [self handleResults:data];
+            }
+    }];
+    [task resume];
+}
+
+- (void)handleResults: (NSData *)data {
+    NSError *jsonError;
+    NSDictionary *response = [NSJSONSerialization JSONObjectWithData: data options:NSJSONReadingAllowFragments error: &jsonError];
+    if(response) {
+        NSLog(@"%@", [response objectForKey:@"data"]);
+        for(NSDictionary *dataDictionary in response[@"data"]) {
+            [self.menuItems addObject:dataDictionary];
+        }
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.someCollectionView reloadData];
+        });
+    }
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +56,12 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return [self.menuItems count];
+}
+
+-(UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewCell *cell = [collectionView  dequeueReusableCellWithReuseIdentifier:@"Cell" forIndexPath:indexPath];
+    return cell;
+}
 @end
